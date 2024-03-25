89b1e53490c0d1418f7b0dd2e0ff4055440e91af
dfebrywulangsih <dfebrywulangsih03@gmail.com>
2024-03-26 01:26:41 +0700

    perubahan pada script. js

--- script.js
+++ script.js
@@ -26,46 +26,44 @@ const items = [
     { name : "xml", image: "xml.png" },
 ];
 
-//Initial Time
+//inisialisasi waktu
 let seconds = 0,
   minutes = 0;
-//Initial moves and win count
+//inisialiasi perpindahan dan perhitungan menang
 let movesCount = 0,
   winCount = 0;
 
-//For timer
+//timer
 const timeGenerator = () => {
   seconds += 1;
-  //minutes logic
   if (seconds >= 60) {
     minutes += 1;
     seconds = 0;
   }
-  //format time before displaying
+  //format timer sebelum ditampilkan
   let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
   let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
   timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
 };
 
-//For calculating moves
+//kalkulasi perpindahan
 const movesCounter = () => {
   movesCount += 1;
   moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
 
-//Pick random objects from the items array
+//Pick random objects dari items array
 const generateRandom = (size = 4) => {
   //temporary array
   let tempArray = [...items];
-  //initializes cardValues array
+  //inisialisasi cardValues array
   let cardValues = [];
-  //size should be double (4*4 matrix)/2 since pairs of objects would exist
+  //size berukuran (4*4 matrix) /2 pasangan
   size = (size * size) / 2;
   //Random object selection
   for (let i = 0; i < size; i++) {
     const randomIndex = Math.floor(Math.random() * tempArray.length);
     cardValues.push(tempArray[randomIndex]);
-    //once selected remove the object from temp array
     tempArray.splice(randomIndex, 1);
   }
   return cardValues;
@@ -74,14 +72,14 @@ const generateRandom = (size = 4) => {
 const matrixGenerator = (cardValues, size = 4) => {
   gameContainer.innerHTML = "";
   cardValues = [...cardValues, ...cardValues];
-  //simple shuffle
+  //pengacakan
   cardValues.sort(() => Math.random() - 0.5);
   for (let i = 0; i < size * size; i++) {
     /*
         Create Cards
-        before => front side (contains question mark)
-        after => back side (contains actual image);
-        data-card-values is a custom attribute which stores the names of the cards to match later
+        before => front side (kartu tertutup)
+        after => back side (berisi gambar);
+        data-card-values berisi atribut acak yang mengandung elemen gambar untuk dicocokkan
       */
     gameContainer.innerHTML += `
      <div class="card-container" data-card-value="${cardValues[i].name}">
@@ -98,39 +96,35 @@ const matrixGenerator = (cardValues, size = 4) => {
   cards = document.querySelectorAll(".card-container");
   cards.forEach((card) => {
     card.addEventListener("click", () => {
-      //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
+      //Jika kartu yang dipilih belum cocok, maka jalankan saja (misal, kartu yang sudah cocok saat diklik akan diabaikan)
       if (!card.classList.contains("matched")) {
-        //flip the cliked card
+        //membalik kartu yang telah diklik
         card.classList.add("flipped");
-        //if it is the firstcard (!firstCard since firstCard is initially false)
         if (!firstCard) {
-          //so current card will become firstCard
+          //menjadi kartu pertama
           firstCard = card;
-          //current cards value becomes firstCardValue
           firstCardValue = card.getAttribute("data-card-value");
         } else {
-          //increment moves since user selected second card
           movesCounter();
-          //secondCard and value
+          //value kartu kedua
           secondCard = card;
           let secondCardValue = card.getAttribute("data-card-value");
           if (firstCardValue == secondCardValue) {
-            //if both cards match add matched class so these cards would beignored next time
+            //apabila kartu cocok maka akan diabaikan nantinya 
             firstCard.classList.add("matched");
             secondCard.classList.add("matched");
-            //set firstCard to false since next card would be first now
+            //setel kartu pertama sbg false karena kartu selanjutnya akan jadi yang pertama 
             firstCard = false;
-            //winCount increment as user found a correct match
+            //poin menang bertambah
             winCount += 1;
-            //check if winCount ==half of cardValues
             if (winCount == Math.floor(cardValues.length / 2)) {
-              result.innerHTML = `<h2>You Won</h2>
+              result.innerHTML = `<h2> You Won </h2>
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
           } else {
-            //if the cards dont match
-            //flip the cards back to normal
+            //apabila kartu tidak cocok 
+            //balik kartu ke posisi semula
             let [tempFirst, tempSecond] = [firstCard, secondCard];
             firstCard = false;
             secondCard = false;
@@ -150,13 +144,13 @@ startButton.addEventListener("click", () => {
   movesCount = 0;
   seconds = 0;
   minutes = 0;
-  //controls amd buttons visibility
+ 
   controls.classList.add("hide");
   stopButton.classList.remove("hide");
   startButton.classList.add("hide");
   //Start timer
   interval = setInterval(timeGenerator, 1000);
-  //initial moves
+  //initial perpindahan 
   moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
   initializer();
 });
@@ -172,7 +166,7 @@ stopButton.addEventListener(
   })
 );
 
-//Initialize values and func calls
+//Initialize nilai and fungsi
 const initializer = () => {
   result.innerText = "";
   winCount = 0;
